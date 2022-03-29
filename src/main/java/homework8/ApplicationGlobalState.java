ed2de4a2146f52c08f5e39ca888095e0c2eb88d9
Vitalii Luzhnov <vitalii@luzhnov.pro>
2022-03-29 14:24:06 +0300

    hw 8 change / Luzhnov 29.03.2022

---
+++ src/main/java/homework8/ApplicationGlobalState.java
@@ -0,0 +1,45 @@
+package homework8;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * Java Core. Homework 8
+ *
+ * @author Vitalii Luzhnov
+ * @version 29.03.2022
+ */
+public final class ApplicationGlobalState {
+
+    private static ApplicationGlobalState INSTANCE;
+    private String selectedCity = null;
+
+    private ApplicationGlobalState() {
+    }
+
+    // Непотокобезопасный код для упрощения
+    public static ApplicationGlobalState getInstance() {
+        if(INSTANCE == null) {
+            INSTANCE = new ApplicationGlobalState();
+        }
+
+        return INSTANCE;
+    }
+
+    public String getSelectedCity() {
+        return selectedCity;
+    }
+
+    public void setSelectedCity(String selectedCity) {
+        this.selectedCity = selectedCity;
+    }
+
+    public String getApiKey() {
+        return "042c1335-f285-4f4c-b25a-1b0f561cacce";
+    }
+
+    public String getApiKeyForCity() {
+        return "HdFtWAHAqnCKgYczZGqfKIEjiAl65MhC";
+    }
+}
